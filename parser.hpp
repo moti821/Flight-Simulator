080acecef0144b26b8198ba235c131402364102a
moti821 <moti821@gmail.com>
2022-06-03 11:18:55 +0300

    mor clean

--- parser.hpp
+++ parser.hpp
@@ -7,17 +7,7 @@ class Parser
 public:
     Parser();
     ~Parser();
-    // {
-    // delete commands["openDataServer"];
-    // delete commands["connect"];
-    // delete commands["var"];
-    // delete commands["equal"];
-    // delete commands["while"];
-    // delete commands["print"];
-    // delete commands["sleep"];
-
-    // }
-
+    
     void creat_hash();
     Command* parse(std::vector<std::string>&);
     std::string delete_space(std::string);
