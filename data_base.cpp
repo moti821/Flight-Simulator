a6dc976b08a4a7883149ff8b3d292df67546db60
moti821 <moti821@gmail.com>
2022-08-17 16:39:42 +0300

    data base

--- data_base.cpp
+++ data_base.cpp
@@ -1,21 +1,20 @@
 #include <mutex>
 #include "data_base.hpp"
 
-DataBase* DataBase::instance = 0;
+DataBase *DataBase::instance = 0;
 std::unordered_map<std::string, double> DataBase::symbol_table;
 std::mutex my_lock;
 
-DataBase* DataBase::get_instance()
+DataBase *DataBase::get_instance()
 {
-    if(instance == 0)
+    if (instance == 0)
     {
-        DataBase* instance = new DataBase;
+        DataBase *instance = new DataBase;
     };
     return instance;
 }
 
-DataBase::DataBase(){}
-
+DataBase::DataBase() {}
 
 void DataBase::insert_value(std::string var, double value)
 {
@@ -27,7 +26,7 @@ void DataBase::insert_value(std::string var, double value)
 double DataBase::get_value(std::string var)
 {
     my_lock.lock();
-    double value =  symbol_table[var];
+    double value = symbol_table[var];
     my_lock.unlock();
     return value;
 }
--- data_base.hpp
+++ data_base.hpp
@@ -1,17 +1,15 @@
-# pragma once
+#pragma once
 #include <unordered_map>
-
 
 class DataBase
 {
 public:
-    static DataBase* get_instance();
+    static DataBase *get_instance();
     void insert_value(std::string var, double value);
     double get_value(std::string var);
 
 private:
-    static DataBase* instance;
+    static DataBase *instance;
     DataBase();
-    
     static std::unordered_map<std::string, double> symbol_table;
 };
